21f2dad30a217c02c84d90c00d965029d417b380
zhaojw30 <zhaojw30@ziroom.com>
2017-05-05 14:10:15 +0800

    添加栈溢出实验

---
+++ src/main/java/com/test/JavaVMStackSOF.java
@@ -0,0 +1,22 @@
+package com.test;
+
+/**
+ * VMArgs：-verbose:gc -Xss128k
+ * Created by alexdrum on 2017/5/5.
+ */
+public class JavaVMStackSOF {
+    private int stackLength = 1;
+    public void stackLeak(){
+        stackLength++;
+        stackLeak();
+    }
+
+    public static void main(String[] args){
+        JavaVMStackSOF oom = new JavaVMStackSOF();
+        try{
+            oom.stackLeak();
+        }catch (Exception e){
+            System.out.println("stack length:"+oom.stackLength);
+        }
+    }
+}
